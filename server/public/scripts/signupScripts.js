5dd19d703fd8b41400ca4b16dcc49287b15c1755
Math33t3 <92299723+Math33t3@users.noreply.github.com>
2023-08-17 11:22:07 +0200

    flyttet nogle js filer

---
+++ server/public/scripts/signupScripts.js
@@ -0,0 +1,49 @@
+
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const username = form.elements.username.value;
+    const password = form.elements.password.value;
+
+    const errorElement = document.getElementById("error");
+    errorElement.textContent = "";
+
+    const credentials = {
+      username: username,
+      password: password
+    };
+
+    fetch("http://localhost:8080/auth/register", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+    .then(function(res) {
+      if (!res.ok || res.status >= 400) {
+        throw new Error("Sign up failed.");
+      }
+      return res.json();
+    })
+    .then(function(data) {
+      if (data && data.loggedIn) {
+        const socket = io(); 
+
+        socket.on("connect", function() {
+          redirectToHome();
+        });
+      } else if (data && data.status) {
+        errorElement.textContent = data.status;
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  });
+
+  function redirectToHome() {
+    window.location.href = 'http://localhost:8080/';
+  }
